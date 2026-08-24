7f0ba25949e57d91384e1883074cacf2b8a16328
noumanTunda <ntunda4000@gmail.com>
2026-08-24 14:52:35 +0300

    Feat: Added Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Sign In | PPRA ICT Equipment Portal',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Create Account | PPRA ICT Equipment Portal',
+  },
+  {
+    path: '**',
+    redirectTo: '/login',
+  },
+];
