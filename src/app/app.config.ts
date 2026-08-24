b37f1583a5467d317308d03135ca4a4925317572
noumanTunda <ntunda4000@gmail.com>
2026-08-24 14:51:45 +0300

    Feat: Added Routers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideClientHydration(),
+  ],
 };
