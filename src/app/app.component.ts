6749003b45675305937cf7ad16d617ededdf7167
noumanTunda <ntunda4000@gmail.com>
2026-08-26 16:17:36 +0300

    Feat: Updated App Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'PPRA ICT Equipment Issue & Return Portal';
+  title = 'ICT Equipment Issue & Return Portal';
 }
